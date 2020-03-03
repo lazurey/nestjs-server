61731e5562408d9681513124a0c3521d4c63028a
sause <lazureys@gmail.com>
2020-03-03 13:28:16 +0800

    all tests passed version

--- src/moment/moment.controller.spec.ts
+++ src/moment/moment.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MomentController } from './moment.controller';
+import { MomentService } from './moment.service';
+import { MomentMemoryRepository } from './moment.repository';
 
 describe('Moment Controller', () => {
   let controller: MomentController;
@@ -7,6 +9,13 @@ describe('Moment Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MomentController],
+      providers: [
+        MomentService,
+        {
+          provide: 'momentDa',
+          useClass: MomentMemoryRepository,
+        }
+      ],
     }).compile();
 
     controller = module.get<MomentController>(MomentController);
--- src/moment/moment.service.spec.ts
+++ src/moment/moment.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MomentService } from './moment.service';
+import { MomentMemoryRepository } from './moment.repository';
 
 describe('MomentService', () => {
   let service: MomentService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MomentService],
+      providers: [
+        MomentService,
+        {
+          provide: 'momentDa',
+          useClass: MomentMemoryRepository,
+        }
+      ],
     }).compile();
 
     service = module.get<MomentService>(MomentService);
