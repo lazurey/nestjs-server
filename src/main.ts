79929e2cb95a6d247a089929d20690dcfd56e887
jiangtangbi <tbjiang@thoughtworks.com>
2020-03-05 15:32:42 +0800

    [bill] add helmet

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import {AppModule} from './app.module';
 import {PagedQueryPipe} from './paged-query.pipe';
 import {JwtAuthGuard} from "./common/guards/jwt-auth.guard";
 import {ValidationPipe} from "@nestjs/common";
-
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new PagedQueryPipe(), new ValidationPipe());
-  app.useGlobalGuards(new JwtAuthGuard())
+  app.useGlobalGuards(new JwtAuthGuard());
+  app.use(helmet());
   await app.listen(3000);
 }
 
